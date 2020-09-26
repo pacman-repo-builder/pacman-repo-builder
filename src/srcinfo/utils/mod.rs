54d5a9f2db84f476a4cd807344daf3a103164dce
khai96_ <hvksmr1996@gmail.com>
2020-09-26 19:25:44 +0700

    Create utils::split_str_once

--- src/srcinfo/utils/mod.rs
+++ src/srcinfo/utils/mod.rs
@@ -1,3 +1,13 @@
+pub fn split_str_once(text: &str, when: impl Fn(char) -> bool) -> (&str, &str) {
+    for (index, current_char) in text.char_indices() {
+        if when(current_char) {
+            return (&text[0..index], &text[index..]);
+        }
+    }
+
+    (text, "")
+}
+
 pub fn extract_value_from_line<'a>(prefix: &str, line: &'a str) -> Option<&'a str> {
     let line = line.trim();
     if !line.starts_with(prefix) {
@@ -11,14 +21,10 @@ pub fn extract_value_from_line<'a>(prefix: &str, line: &'a str) -> Option<&'a st
 }
 
 pub fn extract_pkgname_prefix(text: &str) -> (&str, &str) {
-    for (index, current_char) in text.char_indices() {
-        match current_char {
-            'a'..='z' | 'A'..='Z' | '@' | '.' | '_' | '+' | '-' => continue,
-            _ => return (&text[0..index], &text[index..]),
-        }
-    }
-
-    (text, "")
+    split_str_once(text, |current_char| match current_char {
+        'a'..='z' | 'A'..='Z' | '@' | '.' | '_' | '+' | '-' => false,
+        _ => true,
+    })
 }
 
 #[cfg(test)]
