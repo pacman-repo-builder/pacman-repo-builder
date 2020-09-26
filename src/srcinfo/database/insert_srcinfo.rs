19d38e4d99762d37a3b0b3982cba59ed0d5a330f
khai96_ <hvksmr1996@gmail.com>
2020-09-27 05:31:52 +0700

    Move insert_srcinfo to its own module

---
+++ src/srcinfo/database/insert_srcinfo.rs
@@ -0,0 +1,61 @@
+use super::super::SrcInfo;
+use super::{
+    text_wrapper::{PkgBase, PkgName},
+    Database,
+};
+use pipe_trait::*;
+use std::collections::HashSet;
+
+impl<'a> Database<PkgBase<'a>, PkgName<'a>, &'a str> {
+    pub fn insert_srcinfo(
+        &'a mut self,
+        srcinfo: &'a SrcInfo<&'a str>,
+    ) -> Result<Option<(SrcInfo<&'a str>, HashSet<PkgName>)>, String> {
+        let pkgbase = srcinfo
+            .pkgbase()
+            .ok_or_else(|| "missing pkgbase".to_string())?
+            .pipe(PkgBase);
+
+        let removed_srcinfo = self.infos.insert(pkgbase, *srcinfo);
+
+        let dependency_list = if let Some(dependency_list) = self.dependencies.get_mut(&pkgbase) {
+            dependency_list
+        } else {
+            self.dependencies.insert(pkgbase, Default::default());
+            self.dependencies.get_mut(&pkgbase).unwrap()
+        };
+
+        let mut names_value = HashSet::<PkgName>::new();
+
+        for pkgname in srcinfo.pkgname() {
+            let pkgname = PkgName(pkgname);
+
+            names_value.insert(pkgname);
+            self.bases.insert(pkgname, pkgbase);
+
+            for dependency in srcinfo.all_required_dependencies() {
+                let bases = &self.bases;
+                let dependency_pkgbase = dependency
+                    .name()
+                    .pipe(PkgName)
+                    .pipe_ref(|name| bases.get(name));
+                if let Some(dependency_pkgbase) = dependency_pkgbase {
+                    dependency_list.insert(*dependency_pkgbase);
+                    self.list
+                        .insert(pkgbase.to_string(), dependency_pkgbase.to_string());
+                }
+            }
+        }
+
+        let removed_names = self.names.insert(pkgbase, names_value);
+
+        Ok(match (removed_srcinfo, removed_names) {
+            (None, None) => None,
+            (Some(srcinfo), Some(names)) => Some((srcinfo, names)),
+            (srcinfo, ref names) => {
+                dbg!(srcinfo, names);
+                panic!("impossible state reached: {:?} {:?}", srcinfo, names);
+            }
+        })
+    }
+}
