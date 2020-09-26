a367031f8f61568c0f9a7db0f7ac78ae8e0dde93
khai96_ <hvksmr1996@gmail.com>
2020-09-26 13:06:17 +0700

    Reduce duplication

--- src/srcinfo/mod.rs
+++ src/srcinfo/mod.rs
@@ -8,28 +8,33 @@ use version::Version;
 #[derive(Debug, Copy, Clone)]
 pub struct SrcInfo<Text: AsRef<str>>(pub Text);
 
+macro_rules! line_extractor {
+    ($prefix:expr) => {
+        |line| extract_value_from_line($prefix, line)
+    };
+}
+
 impl<Text: AsRef<str>> SrcInfo<Text> {
     fn lines(&self) -> Lines {
         self.0.as_ref().lines()
     }
 
     pub fn pkgname(&self) -> Option<&str> {
-        self.lines()
-            .find_map(|line| extract_value_from_line("pkgname", line))
+        self.lines().find_map(line_extractor!("pkgname"))
     }
 
     pub fn version(&self) -> Result<Version<&str, &str, &str>, &'static str> {
         let pkgver = self
             .lines()
-            .find_map(|line| extract_value_from_line("pkgver", line))
+            .find_map(line_extractor!("pkgver"))
             .ok_or("missing pkgver")?;
         let pkgrel = self
             .lines()
-            .find_map(|line| extract_value_from_line("pkgrel", line))
+            .find_map(line_extractor!("pkgrel"))
             .ok_or("missing pkgrel")?;
         let epoch = self
             .lines()
-            .find_map(|line| extract_value_from_line("epoch", line))
+            .find_map(line_extractor!("epoch"))
             .unwrap_or("");
         Ok(Version {
             pkgver,
