f930c4371b92172c6f27406a3fcab1b9645acc8d
khai96_ <hvksmr1996@gmail.com>
2020-09-26 15:40:13 +0700

    Create UnreasonedDependency::into_reasoned_dependency

--- src/srcinfo/unreasoned_dependency.rs
+++ src/srcinfo/unreasoned_dependency.rs
@@ -1,7 +1,19 @@
+use super::reasoned_dependency::ReasonedDependency;
+
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub struct UnreasonedDependency<Name: AsRef<str>>(pub Name);
 
 impl<Name: AsRef<str>> UnreasonedDependency<Name> {
+    pub fn into_reasoned_dependency<Reason: AsRef<str>>(
+        self,
+        reason: Option<Reason>,
+    ) -> ReasonedDependency<Name, Reason> {
+        ReasonedDependency {
+            name: self.0,
+            reason,
+        }
+    }
+
     pub fn as_str(&self) -> UnreasonedDependency<&str> {
         UnreasonedDependency(self.name())
     }
