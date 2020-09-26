70e49ed50264999b54850ea94878d4babc1059ad
khai96_ <hvksmr1996@gmail.com>
2020-09-27 04:13:55 +0700

    Add pkgbase to Database

--- src/srcinfo/database/mod.rs
+++ src/srcinfo/database/mod.rs
@@ -8,19 +8,22 @@ use std::{
 };
 
 #[derive(Debug, Default)]
-pub struct Database<PkgName, Range, SrcInfoContent>
+pub struct Database<PkgBase, PkgName, Range, SrcInfoContent>
 where
+    PkgBase: AsRef<str> + Hash + Eq + Clone,
     PkgName: AsRef<str> + Hash + Eq + Clone,
     Range: AsRef<str>,
     SrcInfoContent: AsRef<str>,
 {
+    names: HashMap<PkgBase, HashSet<PkgName>>,
     infos: HashMap<PkgName, SrcInfo<SrcInfoContent>>,
-    dependencies: HashMap<PkgName, HashSet<Dependency<PkgName, Range>>>,
+    dependencies: HashMap<PkgBase, HashSet<Dependency<PkgBase, Range>>>,
     list: PackageList,
 }
 
-impl<PkgName, Range, SrcInfoContent> Database<PkgName, Range, SrcInfoContent>
+impl<PkgBase, PkgName, Range, SrcInfoContent> Database<PkgBase, PkgName, Range, SrcInfoContent>
 where
+    PkgBase: AsRef<str> + Default + Hash + Eq + Clone,
     PkgName: AsRef<str> + Default + Hash + Eq + Clone,
     Range: AsRef<str> + Default,
     SrcInfoContent: AsRef<str> + Default,
@@ -33,7 +36,7 @@ where
         &self.infos
     }
 
-    pub fn dependencies(&self) -> &HashMap<PkgName, HashSet<Dependency<PkgName, Range>>> {
+    pub fn dependencies(&self) -> &HashMap<PkgBase, HashSet<Dependency<PkgBase, Range>>> {
         &self.dependencies
     }
 
@@ -42,7 +45,7 @@ where
     }
 }
 
-impl<'a> Database<&'a str, &'a str, &'a str> {
+impl<'a> Database<&'a str, &'a str, &'a str, &'a str> {
     pub fn insert_srcinfo(
         &'a mut self,
         srcinfo: &'a SrcInfo<&'a str>,
