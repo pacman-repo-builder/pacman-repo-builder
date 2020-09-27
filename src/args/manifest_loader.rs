5c943f9478eb27d12cff568515d66051e3c2ecfb
khai96_ <hvksmr1996@gmail.com>
2020-09-27 22:39:51 +0700

    Create ManifestLoader::load

--- src/args/manifest_loader.rs
+++ src/args/manifest_loader.rs
@@ -1,19 +1,25 @@
 use super::super::manifest::Manifest;
 use pipe_trait::*;
-use std::{fs::File, str::FromStr};
+use std::{fs::File, path::Path, str::FromStr};
 
 #[derive(Debug)]
 pub struct ManifestLoader(pub Manifest);
 
-impl FromStr for ManifestLoader {
-    type Err = String;
-
-    fn from_str(file: &str) -> Result<Self, Self::Err> {
+impl ManifestLoader {
+    pub fn load(file: &Path) -> Result<Self, String> {
         file.pipe(File::open)
             .map_err(|error| format!("cannot open {:?} as a file: {}", file, error))?
             .pipe(serde_yaml::from_reader::<_, Manifest>)
             .map_err(|error| format!("cannot deserialize {:?} as manifest: {}", file, error))?
             .pipe(ManifestLoader)
             .pipe(Ok)
+    }
+}
+
+impl FromStr for ManifestLoader {
+    type Err = String;
+
+    fn from_str(file: &str) -> Result<Self, Self::Err> {
+        ManifestLoader::load(file.as_ref())
     }
 }
