b6ccc56cb1fdc13250f2b000bc7de4861576e321
khai96_ <hvksmr1996@gmail.com>
2020-09-27 11:58:38 +0700

    Create list command

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,22 @@
 use argh::*;
 
-#[derive(FromArgs)]
+#[derive(Debug, FromArgs)]
 #[argh(description = "Build a custom pacman repository from a collection of PKGBUILD directories")]
-pub struct Args {}
+pub struct Args {
+    #[argh(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Debug, FromArgs)]
+#[argh(subcommand)]
+pub enum Command {
+    List(ListArgs),
+}
+
+#[derive(Debug, FromArgs)]
+#[argh(
+    subcommand,
+    name = "list",
+    description = "List packages in build order"
+)]
+pub struct ListArgs {}
