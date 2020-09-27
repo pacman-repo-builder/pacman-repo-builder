519701de51407dab48535b10e726d101f71caa6e
khai96_ <hvksmr1996@gmail.com>
2020-09-27 11:58:14 +0700

    Log args

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,13 @@ pub mod srcinfo;
 pub mod utils;
 
 pub fn main() {
+    let args: args::Args = argh::from_env();
+    dbg!(args);
     unimplemented!()
 }
 
 pub use alpm;
+pub use argh;
 pub use num_bigint;
 pub use num_traits;
 pub use topological_sort;
