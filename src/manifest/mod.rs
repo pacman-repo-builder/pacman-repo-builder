90040ca8879009a5942060688701a0bdc11ffa65
khai96_ <hvksmr1996@gmail.com>
2020-10-02 10:10:53 +0700

    Create Manifest::resolve_member

--- src/manifest/mod.rs
+++ src/manifest/mod.rs
@@ -9,7 +9,7 @@ pub use member::Member;
 pub use repository::{concat_repository_options, Repository};
 
 use serde::{Deserialize, Serialize};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Default, Serialize, Deserialize, Eq, PartialEq)]
 #[serde(rename_all = "kebab-case")]
@@ -24,6 +24,17 @@ impl<P: AsRef<Path>> Manifest<P> {
         Manifest {
             global_settings: self.global_settings.as_ref().map(GlobalSettings::as_path),
             members: self.members.iter().map(Member::as_path).collect(),
+        }
+    }
+
+    pub fn resolve_members(&self) -> Vec<Member<PathBuf>> {
+        if let Some(global_settings) = &self.global_settings {
+            self.members
+                .iter()
+                .map(|x| x.resolve(&global_settings))
+                .collect()
+        } else {
+            self.members.iter().map(Member::to_path_buf).collect()
         }
     }
 }
