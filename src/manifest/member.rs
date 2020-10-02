5707582a4ed9ebbeac8ba198d5b21e2c8cde54c1
khai96_ <hvksmr1996@gmail.com>
2020-10-02 09:34:06 +0700

    Create Member::resolve

--- src/manifest/member.rs
+++ src/manifest/member.rs
@@ -1,6 +1,6 @@
-use super::{BuildMetadata, Repository};
+use super::{concat_repository_options, BuildMetadata, GlobalSettings, Repository};
 use serde::{Deserialize, Serialize};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Default, Serialize, Deserialize, Eq, PartialEq)]
 #[serde(rename_all = "kebab-case")]
@@ -18,6 +18,28 @@ impl<P: AsRef<Path>> Member<P> {
             directory: self.directory.as_ref(),
             read_build_metadata: self.read_build_metadata,
             repository: self.repository.as_ref().map(|x| x.as_path()),
+        }
+    }
+
+    pub fn resolve<Q: AsRef<Path>>(&self, global_settings: &GlobalSettings<Q>) -> Member<PathBuf> {
+        Member {
+            directory: if let Some(container) = &global_settings.container {
+                container.as_ref().join(self.directory.as_ref())
+            } else {
+                self.directory.as_ref().to_path_buf()
+            },
+
+            read_build_metadata: self
+                .read_build_metadata
+                .or(global_settings.read_build_metadata),
+
+            repository: concat_repository_options(
+                self.repository.as_ref().map(Repository::to_path_buf),
+                global_settings
+                    .repository
+                    .as_ref()
+                    .map(Repository::to_path_buf),
+            ),
         }
     }
 }
