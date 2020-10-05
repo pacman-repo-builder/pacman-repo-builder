781cde9e02edcd88d78ab4373ccbcb8adf2c78fb
khai96_ <hvksmr1996@gmail.com>
2020-10-05 14:34:46 +0700

    Test utils::outdated_packages

--- src/utils/outdated_packages.rs
+++ src/utils/outdated_packages.rs
@@ -7,3 +7,55 @@ pub fn outdated_packages<'a, Latest: ToString>(
         .map(|latest| (latest.to_string(), latest))
         .filter(move |(filename, _)| !current_packages.contains(filename))
 }
+
+#[test]
+fn test() {
+    use super::PackageFileName;
+
+    let latest_packages = [
+        PackageFileName {
+            pkgname: "abc",
+            version: "1.2.3-4",
+            arch: "x86_64",
+        },
+        PackageFileName {
+            pkgname: "def",
+            version: "4.3.2-1",
+            arch: "any",
+        },
+        PackageFileName {
+            pkgname: "ghi",
+            version: "0.0.0-1",
+            arch: "i686",
+        },
+    ];
+
+    let current_packages = [
+        "abc-1.2.3-4-x86_64.pkg.tar.zst".to_string(),
+        "def-1.2.3-4-any.pkg.tar.zst".to_string(),
+        "ghi-0.0.0-1-x86_64.pkg.tar.zst".to_string(),
+    ];
+
+    let actual: Vec<_> = outdated_packages(&latest_packages, &current_packages).collect();
+
+    let expected = [
+        (
+            "def-4.3.2-1-any.pkg.tar.zst".to_string(),
+            &PackageFileName {
+                pkgname: "def",
+                version: "4.3.2-1",
+                arch: "any",
+            },
+        ),
+        (
+            "ghi-0.0.0-1-i686.pkg.tar.zst".to_string(),
+            &PackageFileName {
+                pkgname: "ghi",
+                version: "0.0.0-1",
+                arch: "i686",
+            },
+        ),
+    ];
+
+    assert_eq!(&actual, &expected);
+}
