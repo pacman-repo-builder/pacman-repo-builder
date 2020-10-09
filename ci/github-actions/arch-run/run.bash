458a991999a00d95efc4527b1dbc997b910e6600
khai96_ <hvksmr1996@gmail.com>
2020-10-09 15:31:28 +0700

    Move patching of makepkg to Dockerfile

--- ci/github-actions/arch-run/run.bash
+++ ci/github-actions/arch-run/run.bash
@@ -1,9 +1,3 @@
 #! /bin/bash
 set -o errexit -o pipefail -o nounset
-
-echo '::group::Patching some file...'
-echo 'force makepkg to allow running as root'
-cp -v patches/makepkg /usr/bin/makepkg
-echo '::endgroup::'
-
 eval "$INPUT_COMMAND"
