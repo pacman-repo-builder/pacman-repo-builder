3eee7c6c950ec1113f31e82a4a8e8be030582ca8
khai96_ <hvksmr1996@gmail.com>
2020-10-12 18:25:29 +0700

    Use create_makepkg_command in read_srcinfo_from_pkgbuild

--- src/utils/read_srcinfo_from_pkgbuild.rs
+++ src/utils/read_srcinfo_from_pkgbuild.rs
@@ -1,8 +1,9 @@
+use super::create_makepkg_command;
 use pipe_trait::*;
-use std::{path::Path, process::Command};
+use std::path::Path;
 
 pub fn read_srcinfo_from_pkgbuild(directory: &Path) -> Result<String, String> {
-    let output = Command::new("makepkg")
+    let output = create_makepkg_command()
         .current_dir(directory)
         .arg("--printsrcinfo")
         .output()
