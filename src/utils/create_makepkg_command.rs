16057bd198396b93eaa835ccaa5dc6d5a070fa0c
khai96_ <hvksmr1996@gmail.com>
2020-10-13 19:59:04 +0700

    Hide more environment variables

--- src/utils/create_makepkg_command.rs
+++ src/utils/create_makepkg_command.rs
@@ -3,6 +3,7 @@ use std::process::Command;
 pub fn create_makepkg_command() -> Command {
     let mut command = Command::new("makepkg");
     command
+        .env_remove("PACMAN")
         .env_remove("MAKEPKG_CONF")
         .env_remove("PKGDEST")
         .env_remove("SRCDEST")
@@ -10,6 +11,9 @@ pub fn create_makepkg_command() -> Command {
         .env_remove("PACKAGER")
         .env_remove("SRCPKGDEST")
         .env_remove("BUILDDIR")
+        .env_remove("GNUPGHOME")
+        .env_remove("GPGKEY")
+        .env_remove("SOURCE_DATE_EPOCH")
         .env("PKGEXT", ".pkg.tar.zst")
         .env("SRCEXT", ".src.tar.gz");
     command
