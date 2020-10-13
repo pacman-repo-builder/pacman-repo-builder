668451be3e20c24658420c08dcdfb8fcb285722c
khai96_ <hvksmr1996@gmail.com>
2020-10-13 10:10:26 +0700

    Create MayEnv utility trait

---
+++ src/utils/may_env.rs
@@ -0,0 +1,24 @@
+use std::{ffi::OsStr, process::Command};
+
+pub trait MayEnv<Key, Value>: Sized {
+    fn with_env(self, key: Key, value: Value) -> Self;
+
+    fn may_env(self, key: Key, value: Option<Value>) -> Self {
+        if let Some(value) = value {
+            self.with_env(key, value)
+        } else {
+            self
+        }
+    }
+}
+
+impl<Key, Value> MayEnv<Key, Value> for Command
+where
+    Key: AsRef<OsStr>,
+    Value: AsRef<OsStr>,
+{
+    fn with_env(mut self, key: Key, value: Value) -> Self {
+        self.env(key, value);
+        self
+    }
+}
