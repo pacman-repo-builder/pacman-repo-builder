1ad4859f1cbe89a7191b26e8f5a25dc6f103dd91
khai96_ <hvksmr1996@gmail.com>
2020-10-13 10:38:58 +0700

    refactor: Remove generic from CommandExtra

--- src/utils/command_extra.rs
+++ src/utils/command_extra.rs
@@ -1,9 +1,9 @@
 use std::{ffi::OsStr, process::Command};
 
-pub trait CommandExtra<Key, Value>: Sized {
-    fn with_env(self, key: Key, value: Value) -> Self;
+pub trait CommandExtra: Sized {
+    fn with_env(self, key: impl AsRef<OsStr>, value: impl AsRef<OsStr>) -> Self;
 
-    fn may_env(self, key: Key, value: Option<Value>) -> Self {
+    fn may_env(self, key: impl AsRef<OsStr>, value: Option<impl AsRef<OsStr>>) -> Self {
         if let Some(value) = value {
             self.with_env(key, value)
         } else {
@@ -12,12 +12,8 @@ pub trait CommandExtra<Key, Value>: Sized {
     }
 }
 
-impl<Key, Value> CommandExtra<Key, Value> for Command
-where
-    Key: AsRef<OsStr>,
-    Value: AsRef<OsStr>,
-{
-    fn with_env(mut self, key: Key, value: Value) -> Self {
+impl CommandExtra for Command {
+    fn with_env(mut self, key: impl AsRef<OsStr>, value: impl AsRef<OsStr>) -> Self {
         self.env(key, value);
         self
     }
