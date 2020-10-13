280dff11ec70c10162accfdc8ba3a1101e082b32
khai96_ <hvksmr1996@gmail.com>
2020-10-13 10:09:53 +0700

    Hide some environment variables from makepkg

--- src/utils/create_makepkg_command.rs
+++ src/utils/create_makepkg_command.rs
@@ -3,6 +3,13 @@ use std::process::Command;
 pub fn create_makepkg_command() -> Command {
     let mut command = Command::new("makepkg");
     command
+        .env_remove("MAKEPKG_CONF")
+        .env_remove("PKGDEST")
+        .env_remove("SRCDEST")
+        .env_remove("LOGDEST")
+        .env_remove("PACKAGER")
+        .env_remove("SRCPKGDEST")
+        .env_remove("BUILDDIR")
         .env("PKGEXT", ".pkg.tar.zst")
         .env("SRCEXT", ".src.tar.gz");
     command
