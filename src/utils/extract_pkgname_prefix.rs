e6e6335898f14d7993ba6ff36419774a8ec5c96e
khai96_ <hvksmr1996@gmail.com>
2020-10-14 15:32:26 +0700

    refactor: Use matches! macro

--- src/utils/extract_pkgname_prefix.rs
+++ src/utils/extract_pkgname_prefix.rs
@@ -1,10 +1,10 @@
 use super::split_str_once;
 
 pub fn extract_pkgname_prefix(text: &str) -> (&str, &str) {
-    split_str_once(text, |current_char, _| match current_char {
-        'a'..='z' | 'A'..='Z' | '0'..='9' | '@' | '.' | '_' | '+' | '-' => false,
-        _ => true,
-    })
+    split_str_once(
+        text,
+        |current_char, _| !matches!(current_char, 'a'..='z' | 'A'..='Z' | '0'..='9' | '@' | '.' | '_' | '+' | '-'),
+    )
 }
 
 #[test]
