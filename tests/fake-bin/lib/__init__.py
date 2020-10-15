0ff5f10ec22fcbe1832a529aa512efa0f2b0389a
khai96_ <hvksmr1996@gmail.com>
2020-10-15 09:55:07 +0700

    Always use pacman as query processor

--- tests/fake-bin/lib/__init__.py
+++ tests/fake-bin/lib/__init__.py
@@ -3,14 +3,13 @@ from os import environ
 from sys import argv
 import subprocess
 
-def run_fake_command(name: str) -> NoReturn:
+def run_fake_pacman(keyword: str) -> NoReturn:
   args = argv[1:]
 
   if any(x == '--query' or x.startswith('-Q') for x in args):
-    result = subprocess.run([f'/usr/bin/{name}', *args])
+    result = subprocess.run([f'/usr/bin/pacman', *args])
     exit(result.returncode)
 
-  keyword = name.upper()
   with open(environ[f'FAKE_{keyword}_OUTPUT_FILE'], 'w') as output:
     for x in args: output.write(x + '\n')
   exit(int(environ[f'FAKE_{keyword}_EXIT_STATUS']))
