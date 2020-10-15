5872676828f60f5cad2d99059a625986caf6ccdc
khai96_ <hvksmr1996@gmail.com>
2020-10-15 09:26:40 +0700

    Pass program name instead of keyword

--- tests/fake-bin/lib/__init__.py
+++ tests/fake-bin/lib/__init__.py
@@ -2,7 +2,8 @@ from typing import NoReturn
 from os import environ
 from sys import argv
 
-def run_fake_command(keyword: str) -> NoReturn:
+def run_fake_command(name: str) -> NoReturn:
+  keyword = name.upper()
   with open(environ[f'FAKE_{keyword}_OUTPUT_FILE'], 'w') as output:
     for x in argv[1:]: output.write(x + '\n')
   exit(int(environ[f'FAKE_{keyword}_EXIT_STATUS']))
