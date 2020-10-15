7da0ae69481ede33968e912daa11dfa603d9f3c1
khai96_ <hvksmr1996@gmail.com>
2020-10-15 16:24:53 +0700

    Create LIBRARY_PATH and LD_LIBRARY_PATH

--- ci/github-actions/arch-run/run.bash
+++ ci/github-actions/arch-run/run.bash
@@ -4,4 +4,7 @@ set -o errexit -o pipefail -o nounset
 # force makepkg to allow running as root
 cp patches/makepkg /usr/bin/makepkg
 
+export LIBRARY_PATH=/usr/lib
+export LD_LIBRARY_PATH=/usr/lib
+
 eval "$INPUT_COMMAND"
