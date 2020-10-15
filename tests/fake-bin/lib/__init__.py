52b808f44ff58b492868f76b61011cdd5e47af10
khai96_ <hvksmr1996@gmail.com>
2020-10-15 09:40:59 +0700

    Pass -Q/--query to real pacman/yay

--- tests/fake-bin/lib/__init__.py
+++ tests/fake-bin/lib/__init__.py
@@ -1,9 +1,16 @@
 from typing import NoReturn
 from os import environ
 from sys import argv
+import subprocess
 
 def run_fake_command(name: str) -> NoReturn:
+  args = argv[1:]
+
+  if any(x == '--query' or x.startswith('-Q') for x in args):
+    result = subprocess.run([f'/usr/bin/{name}', *args])
+    exit(result.returncode)
+
   keyword = name.upper()
   with open(environ[f'FAKE_{keyword}_OUTPUT_FILE'], 'w') as output:
-    for x in argv[1:]: output.write(x + '\n')
+    for x in args: output.write(x + '\n')
   exit(int(environ[f'FAKE_{keyword}_EXIT_STATUS']))
