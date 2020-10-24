4003a1e113dcaab17f0a7f7858fae5dc0e4de71e
khai96_ <hvksmr1996@gmail.com>
2020-10-24 10:28:00 +0700

    Add tests to outdated for arch-filter

--- tests/outdated.rs
+++ tests/outdated.rs
@@ -78,6 +78,62 @@ test_case!(
     "./expected-output/outdated/simple/details-strict-yaml.stdout.yaml"
 );
 
+test_case!(
+    details_pkgname_arch_filter_any,
+    "arch-filter-any",
+    "pkgname",
+    "./expected-output/outdated/arch-filter-any/details-pkgname.stdout.txt"
+);
+
+test_case!(
+    details_pkg_file_path_arch_filter_any,
+    "arch-filter-any",
+    "pkg-file-path",
+    "./expected-output/outdated/arch-filter-any/details-pkg-file-path.stdout.txt"
+);
+
+test_case!(
+    details_lossy_yaml_arch_filter_any,
+    "arch-filter-any",
+    "lossy-yaml",
+    "./expected-output/outdated/arch-filter-any/details-lossy-yaml.stdout.yaml"
+);
+
+test_case!(
+    details_strict_yaml_arch_filter_any,
+    "arch-filter-any",
+    "strict-yaml",
+    "./expected-output/outdated/arch-filter-any/details-strict-yaml.stdout.yaml"
+);
+
+test_case!(
+    details_pkgname_arch_filter_x86_64,
+    "arch-filter-x86_64",
+    "pkgname",
+    "./expected-output/outdated/arch-filter-x86_64/details-pkgname.stdout.txt"
+);
+
+test_case!(
+    details_pkg_file_path_arch_filter_x86_64,
+    "arch-filter-x86_64",
+    "pkg-file-path",
+    "./expected-output/outdated/arch-filter-x86_64/details-pkg-file-path.stdout.txt"
+);
+
+test_case!(
+    details_lossy_yaml_arch_filter_x86_64,
+    "arch-filter-x86_64",
+    "lossy-yaml",
+    "./expected-output/outdated/arch-filter-x86_64/details-lossy-yaml.stdout.yaml"
+);
+
+test_case!(
+    details_strict_yaml_arch_filter_x86_64,
+    "arch-filter-x86_64",
+    "strict-yaml",
+    "./expected-output/outdated/arch-filter-x86_64/details-strict-yaml.stdout.yaml"
+);
+
 #[test]
 fn validate_yaml_output() {
     use serde_yaml::{from_str, Value};
