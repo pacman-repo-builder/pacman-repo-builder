e66c41d755bbb1e62f9168599e3fb8b3d094f2a0
khai96_ <hvksmr1996@gmail.com>
2021-03-17 18:28:19 +0700

    Create needed functions to detect missing dependencies and their conflicts

--- src/utils/alpm_wrapper.rs
+++ src/utils/alpm_wrapper.rs
@@ -8,6 +8,7 @@ const DATABASE_PATH: &str = "/var/lib/pacman";
 #[derive(Debug)]
 pub struct AlpmWrapper {
     alpm: Alpm,
+    loaded_packages: Vec<LoadedPackageParam>,
 }
 
 impl AlpmWrapper {
@@ -17,7 +18,39 @@ impl AlpmWrapper {
             alpm.register_syncdb(repo.name, SigLevel::NONE)
                 .expect("register syncdb");
         }
-        AlpmWrapper { alpm }
+        AlpmWrapper {
+            alpm,
+            loaded_packages: Default::default(),
+        }
+    }
+
+    pub fn load_package(&mut self, filename: Vec<u8>) {
+        self.loaded_packages.push(LoadedPackageParam { filename })
+    }
+
+    pub fn needed<'a>(&self, packages: impl Iterator<Item = &'a str>) -> InstallationPlan {
+        let local_packages = || self.alpm.localdb().pkgs().into_iter();
+
+        let wanted: Vec<String> = packages
+            .filter(|target| {
+                local_packages().all(|pkg| {
+                    pkg.name() != *target
+                        && pkg.provides().into_iter().all(|pkg| pkg.name() != *target)
+                })
+            })
+            .map(ToString::to_string)
+            .collect();
+
+        let unwanted: Vec<String> = local_packages()
+            .filter(|pkg| {
+                pkg.conflicts()
+                    .into_iter()
+                    .any(|dep| wanted.iter().any(|pkgname| dep.name() == pkgname))
+            })
+            .map(|pkg| pkg.name().to_string())
+            .collect();
+
+        InstallationPlan { wanted, unwanted }
     }
 
     pub fn provides(&self, pkgname: &str) -> bool {
@@ -40,4 +73,15 @@ impl AlpmWrapper {
             .flat_map(|pkg| pkg.provides())
             .any(|pkg| pkg.name() == pkgname)
     }
+}
+
+#[derive(Debug)]
+struct LoadedPackageParam {
+    filename: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub struct InstallationPlan {
+    pub wanted: Vec<String>,
+    pub unwanted: Vec<String>,
 }
